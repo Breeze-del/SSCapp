eaff80b697818d00d33ce3aeecb26c220a1220c8
Liqingfeng <562916584@qq.com>
2018-10-02 10:40:36 +0800

    完成主界面UI和点击事件

--- app/src/main/java/com/example/liqingfeng/sscapp/View/Activity/MainActivity.java
+++ app/src/main/java/com/example/liqingfeng/sscapp/View/Activity/MainActivity.java
@@ -21,14 +21,25 @@ import com.example.liqingfeng.sscapp.R;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private Toolbar toolbar;          //界面顶部栏
+    private FloatingActionButton fab; //界面底部悬浮按钮
+    private DrawerLayout drawer;      //滑动切换导航栏和content
+    private NavigationView navigationView;//导航栏
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
-        Toolbar toolbar = (Toolbar) findViewById( R.id.toolbar );
-        setSupportActionBar( toolbar );
+        init();
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById( R.id.fab );
+    /**
+     * 初化始界面控件和点击事件
+     */
+    private void init() {
+        toolbar = (Toolbar) findViewById( R.id.toolbar );
+        setSupportActionBar( toolbar );
+        //悬浮按钮的点击事件
+        fab = (FloatingActionButton) findViewById( R.id.fab );
         fab.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -36,17 +47,20 @@ public class MainActivity extends AppCompatActivity
                         .setAction( "Action", null ).show();
             }
         } );
-
-        DrawerLayout drawer = (DrawerLayout) findViewById( R.id.drawer_layout );
+        //打开关闭导航栏
+        drawer = (DrawerLayout) findViewById( R.id.drawer_layout );
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close );
         drawer.addDrawerListener( toggle );
         toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById( R.id.nav_view );
+        //导航栏点击事件监听
+        navigationView = (NavigationView) findViewById( R.id.nav_view );
         navigationView.setNavigationItemSelectedListener( this );
     }
 
+    /**
+     * 华东切换导航栏和内容板块
+     */
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById( R.id.drawer_layout );
@@ -57,6 +71,11 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
+    /**
+     *  将你添加的操作栏的item添加到你的布局中去
+     * @param menu
+     * @return
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -64,6 +83,11 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
+    /**
+     *  setting 里面点击事件处理
+     * @param item  点击的是哪一个setting
+     * @return
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -79,26 +103,30 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected( item );
     }
 
+    /**
+     * 导航栏的点击事件
+     * @param item 点击的是哪一个导航栏的item
+     * @return
+     */
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_sportslevel) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_sportsgrade) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_sportsphoto) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_sportsfeedback) {
+
+        } else if (id == R.id.nav_aboutus) {
 
         } else if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_send) {
-
         }
-
         DrawerLayout drawer = (DrawerLayout) findViewById( R.id.drawer_layout );
         drawer.closeDrawer( GravityCompat.START );
         return true;
